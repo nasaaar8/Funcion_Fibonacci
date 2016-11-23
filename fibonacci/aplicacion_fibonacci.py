85582cdc6ee81e9c0911c56dfad907574cffc403
nasaaar8 <nasaaar8@gmail.com>
2016-11-23 11:42:12 +0100

    aplicacion_fibonacci

--- fibonacci/aplicacion_fibonacci.py
+++ fibonacci/aplicacion_fibonacci.py
@@ -3,3 +3,7 @@ Created on 23 nov. 2016
 
 @author: najib
 '''
+from fibonacci_funcion.funcion_fibonacci import fibonacci
+numero=int(input("Escribe un numero entero:"))
+fibonacci=fibonacci(numero)
+print("La funcion de fibonacci del numero %d"%numero,"es %d"%fibonacci)
